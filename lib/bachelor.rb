3ed3a6b3598aa76b7feab03a51d6bddcf07e122a
sam ghoreishi <sam20gh@gmail.com>
2019-02-14 22:56:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -33,7 +33,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_num, contestants|
+    contestants.each do |cont_hash|
+      if cont_hash["hometown"] == hometown
+        return cont_hash["name"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
