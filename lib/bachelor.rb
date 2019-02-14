12cd94b23f5b7475925504fe3243e1cc3a65e66b
sam ghoreishi <sam20gh@gmail.com>
2019-02-14 22:50:23 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,7 +25,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |season_num, contestants|
     contestants.each do |cont_hash|
       if cont_hash["hometown"] == hometown
-        number << hometown(name)
+        number.push(name)
       end
     end
   end
